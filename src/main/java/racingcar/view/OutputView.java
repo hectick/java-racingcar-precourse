0217ee086749d392179b420e4fbbb6692f1f28ee
hectick <chyeon1223@cau.ac.kr>
2022-12-16 23:53:48 +0900

    feat: 입력문구를 출력하는 기능 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,14 @@
+package racingcar.view;
+
+import racingcar.constant.Message;
+
+public class OutputView {
+
+    public static void printGetCarNamesMessage(){
+        System.out.println(Message.GET_CAR_NAMES_MESSAGE);
+    }
+
+    public static void pirntGetTryCountMessage(){
+        System.out.println(Message.GET_TRY_COUNT_MESSAGE);
+    }
+}
